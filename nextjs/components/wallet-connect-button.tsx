d93832374137e2a8a4943ea88a1fd5a4676197b0
aulneau <aulneau@users.noreply.github.com>
2021-11-02 15:55:22 -0500

    chore: more cleanup, bump deps

--- nextjs/components/wallet-connect-button.tsx
+++ nextjs/components/wallet-connect-button.tsx
@@ -1,8 +1,7 @@
-import { useAuth, useAuthOptions } from '@micro-stacks/react';
+import { useAuth } from '@micro-stacks/react';
 
 export const WalletConnectButton = () => {
   const { isSignedIn, handleSignIn, handleSignOut, isLoading } = useAuth();
-  console.log(useAuthOptions());
   return (
     <button onClick={isSignedIn ? () => handleSignOut() : () => handleSignIn({})}>
       {isLoading ? 'Loading...' : isSignedIn ? 'Sign out' : 'Connect Stacks Wallet'}
