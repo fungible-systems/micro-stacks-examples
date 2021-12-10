ec10af124455ac53544f611c5bc63881a40abce2
aulneau <aulneau@users.noreply.github.com>
2021-12-10 17:32:53 -0600

    fix: update examples

--- nextjs/pages/index.tsx
+++ nextjs/pages/index.tsx
@@ -8,11 +8,11 @@ import type { NextPage } from 'next';
 
 const Home: NextPage = () => {
   return (
-    <>
-      <Header />
-      <Devtools />
-      <MainArea />
-    </>
+      <>
+        <Header />
+        <Devtools />
+        <MainArea />
+      </>
   );
 };
 
--- vite-react-ts/src/App.tsx
+++ vite-react-ts/src/App.tsx
@@ -1,45 +1,53 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import { MicroStacksProvider, useAuth, useUser } from "@micro-stacks/react";
+import { WalletConnectButton } from "./wallet-connect-button";
+
+import logo from "./logo.svg";
+import "./App.css";
+
+const Contents = () => {
+  const { isSignedIn } = useAuth();
+  const userData = useUser();
+  if (isSignedIn)
+    return (
+      <>
+        <h1>Welcome!</h1>
+        <h2>{userData.addresses?.mainnet}</h2>
+        <br />
+        <WalletConnectButton />
+      </>
+    );
+  return (
+    <>
+      <h1>pls sign in</h1>
+      <WalletConnectButton />
+    </>
+  );
+};
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
-  )
+    <MicroStacksProvider
+      authOptions={{
+        appDetails: {
+          name: "my cool app",
+          icon: logo,
+        },
+      }}
+    >
+      <div
+        style={{
+          width: "100%",
+          height: "100vh",
+          display: "flex",
+          alignItems: "center",
+          flexDirection: "column",
+          justifyContent: "center",
+        }}
+      >
+        <Contents />
+      </div>
+    </MicroStacksProvider>
+  );
 }
 
-export default App
+export default App;
