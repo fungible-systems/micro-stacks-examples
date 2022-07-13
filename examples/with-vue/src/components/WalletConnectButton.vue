6136e743b337413d3a22a4b26fb696bd54adc6ed
aulneau <aulneau@users.noreply.github.com>
2022-07-13 13:04:30 -0500

    feat: add vue non-ts version

---
+++ examples/with-vue/src/components/WalletConnectButton.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { useAccount, useAuth } from '@micro-stacks/vue';
+
+const auth = $(useAuth());
+const account = $(useAccount());
+
+const onClick = () => {
+  !auth.isSignedIn ? auth.openAuthRequest() : auth.signOut();
+};
+</script>
+
+<template>
+  <h1>{{ account.stxAddress }}</h1>
+  <button type="button" @click="() => onClick()">
+    {{
+      auth.isRequestPending ? 'pending...' : auth.isSignedIn ? 'sign out' : 'connect stacks wallet'
+    }}
+  </button>
+</template>
