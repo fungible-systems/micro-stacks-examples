fd3e14e5a9e6e6f5448bbde4851837c18cd18c7a
aulneau <aulneau@users.noreply.github.com>
2022-07-13 13:46:21 -0500

    chore(with-svelekit): dynamic local url

--- examples/with-sveltekit/src/common/fetchers.ts
+++ examples/with-sveltekit/src/common/fetchers.ts
@@ -1,19 +1,20 @@
-const url = import.meta.env.PROD ? 'https://micro-stacks-sveltekit-example.vercel.app' : 'http://localhost:3000'
+const url = import.meta.env.PROD ? 'https://micro-stacks-sveltekit-example.vercel.app' : `http://${new URL(import.meta.url).host}`;
+
 
 export const onPersistState = async (dehydratedState: string) => {
   const formData = new FormData();
 
-  formData.set("dehydratedState", dehydratedState);
+  formData.set('dehydratedState', dehydratedState);
 
-  await fetch(url + "/api/session/save", {
-    method: "POST",
+  await fetch(url + '/api/session/save', {
+    method: 'POST',
     body: formData,
   });
 };
 
 export const onSignOut = async () => {
-  await fetch(url + "/api/session/destroy", {
-    method: "POST",
+  await fetch(url + '/api/session/destroy', {
+    method: 'POST',
     body: null,
   });
 };
