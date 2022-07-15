66710338f7ffe9b47158da447be6625e05381e54
aulneau <aulneau@users.noreply.github.com>
2022-07-15 17:15:23 -0500

    feat(with-remix): use starTransition for client hydration

--- examples/with-remix/app/entry.client.tsx
+++ examples/with-remix/app/entry.client.tsx
@@ -1,4 +1,15 @@
+import React from 'react';
 import { RemixBrowser } from '@remix-run/react';
 import { hydrateRoot } from 'react-dom/client';
 
-hydrateRoot(document, <RemixBrowser />);
+// https://twitter.com/ryanflorence/status/1547959632663961602
+requestIdleCallback(() => {
+  React.startTransition(() => {
+    hydrateRoot(
+      document,
+      <React.StrictMode>
+        <RemixBrowser />
+      </React.StrictMode>
+    );
+  });
+});
